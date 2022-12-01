dba6749921ab1aac3fa3f4b4506211203cad712a
Arsalan Saif <72790445+arsalansaif@users.noreply.github.com>
2022-12-01 17:47:44 -0500

    Add files via upload

---
+++ YOURCODEHERE.c
@@ -0,0 +1,110 @@
+#include "YOURCODEHERE.h"
+
+/**********************************************************************
+    Function    : lg2pow2
+    Description : this help funciton for you to calculate the bit number
+                  this function is not allowed to modify
+    Input       : pow2 - for example, pow2 is 16
+    Output      : retval - in this example, retval is 4
+***********************************************************************/
+unsigned int lg2pow2(uint64_t pow2){
+  unsigned int retval=0;
+  while(pow2 != 1 && retval < 64){
+    pow2 = pow2 >> 1;
+    ++retval;
+  }
+  return retval;
+}
+
+void  setSizesOffsetsAndMaskFields(cache* acache, unsigned int size, unsigned int assoc, unsigned int blocksize){
+  unsigned int localVAbits=8*sizeof(uint64_t*);
+  if (localVAbits!=64){
+    fprintf(stderr,"Running non-portable code on unsupported platform, terminating. Please use designated machines.\n");
+    exit(-1);
+  }
+  unsigned int numSet;
+  unsigned int sizeIndex;
+
+  acache->numways = assoc;
+  acache->blocksize = blocksize;
+  acache->numsets = (size / blocksize) / assoc;
+  acache->BO = 0;
+
+  // get the log2 of blocksize
+  while (blocksize >= 2) {
+    blocksize /= 2;
+    acache->BO++;
+  }
+
+  sizeIndex = 0;
+  numSet = acache->numsets;
+
+  // get the log2 of numSet
+  while (numSet >= 2) {
+    numSet /= 2;
+    sizeIndex++;
+  }
+
+  acache->VAImask = 0;
+
+  // create index andmask
+  for (int curBitI = 0; curBitI < sizeIndex; ++curBitI) {
+    acache->VAImask |= ((uint64_t)1 << curBitI);
+  }
+
+  acache->TO = acache->BO + sizeIndex;
+  unsigned int sizeTag = localVAbits - acache->TO;
+  acache->VATmask = 0;
+
+  // create tag andmask
+  for (int curBitT = 0; curBitT < sizeTag; ++curBitT) {
+    acache->VATmask |= ((uint64_t)1 << curBitT);
+  }
+}
+
+unsigned long long getindex(cache* acache, unsigned long long address){
+  unsigned long long index;
+  index = address >> acache->BO;
+  index &= acache->VAImask;
+
+  return index;
+}
+
+unsigned long long gettag(cache* acache, unsigned long long address){
+  unsigned long long tag;
+  tag = address >> acache->TO;
+  tag &= acache->VATmask;
+
+  return tag;
+}
+
+void writeback(cache* acache, unsigned int index, unsigned int oldestway){
+  unsigned long long wordSize = sizeof(unsigned long long);
+  unsigned long long address = 0;
+  unsigned long long addressIndex = index << acache->BO;
+  unsigned long long addressTag = acache->sets[index].blocks[waynum].tag << acache->TO;
+  address |= addressIndex;
+  address |= addressTag; // get address value
+
+  for(int curword = 0; curword < (acache->blocksize / wordSize); curword++){
+    unsigned long long word = acache->sets[index].blocks[waynum].datawords[curword];
+    performaccess(acache->nextcache, address, wordSize, 1, word, curword); // send/write data
+    address += wordSize;
+  }
+}
+
+void fill(cache * acache, unsigned int index, unsigned int oldestway, unsigned long long address){
+  unsigned long long wordSize = sizeof(unsigned long long);
+  unsigned long long buildTag = acache->VATmask << acache->TO;
+  unsigned long long buildIndex = acache->VAImask << acache->BO;
+  unsigned long long buildTI = buildTag | buildIndex;
+  address &= buildTI; // get address value
+
+  for(int curword = 0; curword < (acache->blocksize / wordSize); curword++){
+    unsigned long long word = performaccess(acache->nextcache, address, wordSize, 0, 0, curword); // receive/read data
+    acache->sets[index].blocks[waynum].datawords[curword] = word; // update datawords content
+    unsigned long long tag = gettag(acache, address);
+    acache->sets[index].blocks[waynum].tag = tag; // update tag value
+    address += wordSize;
+  }
+}
